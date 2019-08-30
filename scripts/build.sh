5e8760d7883577aeea62d735c2057b4264821bbb
RichieEscarez <rescarez@google.com>
2019-08-30 15:46:01 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -81,7 +81,7 @@ then
 
   # Retrieve the repo/fork URL
   #CLONEURL=$(echo "$INCOMING_HOOK_BODY" | grep -o  '\"clone_url\"\:\".*\.git\"\,\"svn_url\"' | sed -e 's/.*\"clone_url\"\:\"//;s/\.git\".*/\.git/;s/https\:\/\/github\.com\/knative\/docs.git//' || true)
-  CLONEURL=$(echo "$INCOMING_HOOK_BODY" | grep -o -m 1 '\"label\"\:\".*\"\,\"ref\"' | sed -e 's/\"label\"\:\"knative\:.*//;s/\"label\"\:\"//;s/\"\,\"ref\"//' || true)
+  CLONEURL=$(echo "$INCOMING_HOOK_BODY" | grep -o -m 1 '\"label\"\:\".*\"\,\"ref\"' | sed -e 's/\"label\"\:\"knative\:.*//;s/\"label\"\:\"//;s/\"\,\"ref\".*//' || true)
   echo 'CLONEURL:' "$CLONEURL"
   FORK=$(echo "$CLONEURL" | sed -e 's/https\:\/\/github.com\///;s/\/docs.git//')
   echo 'fork is:' "$FORK"
